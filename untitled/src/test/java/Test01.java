571e37c11b92f8ef0ec6fe4008c8a6df74dca665
james <james@gmail.com>
2022-05-24 15:59:01 +0800

    32

--- untitled/src/test/java/Test01.java
+++ untitled/src/test/java/Test01.java
@@ -7,8 +7,21 @@ import org.junit.Test;
 public class Test01 {
     @Test
     public void test() {
+        int count = 0;
         for (int i = 2; i <= 100; i++) {
-
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                count++;
+                System.out.print(i + " ");
+            }
         }
+        System.out.println();
+        System.out.println(count);
     }
 }
